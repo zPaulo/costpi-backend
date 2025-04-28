563f4fbf01085c78ff74d82d6f964f21091f9a9b
zPaulo <paulotjs147@hotmail.com>
2025-04-28 00:05:53 -0300

    ajustando o allowed_hosts

--- src/apps/settings.py
+++ src/apps/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-secret-key')
 
-# SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
